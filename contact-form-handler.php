213bb35b9edfb738afabc6843dfd5d3c2f87afb3
Diguez <108573918+Diguez@users.noreply.github.com>
2022-07-10 03:23:53 +0100

    Update contact-form-handler.php

--- contact-form-handler.php
+++ contact-form-handler.php
@@ -1,15 +1,29 @@
-<form
-  action="https://formspree.io/f/mwkydngz"
-  method="POST"
->
-  <label>
-    Your email:
-    <input type="email" name="email">
-  </label>
-  <label>
-    Your message:
-    <textarea name="message"></textarea>
-  </label>
-  <!-- your other form fields go here -->
-  <button type="submit">Send</button>
-</form>
+<?php
+    $name = $_POST['name'];
+    $visitor_email = $_POST['email'];
+    $subject = $_POST['subject'];
+    $message = $_POST['message'];
+
+
+    $email_from = 'info@easytutorialspro.com';
+
+    $email_subject = "New Form Submission";
+    
+    $email_body =   "User Name: $name.\n".
+                    "User Email: $visitor_email.\n".
+                    "Subject: $subject.\n".
+                    "User Message: $message.\n";
+
+
+    $to = "avinash6252@gmail.com";
+
+    $headers = "From: $email_from \r\n";
+
+    $headers .= "Reply-To: $visitor_email \r\n";
+
+    mail($to,$email_subject,$email_body,$headers);
+
+    header("Location: contact.html");
+
+
+?>
